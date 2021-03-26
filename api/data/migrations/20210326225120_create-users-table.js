5c31403aa59e0d53d49f717b94428febedb1ae78
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-26 17:58:39 -0500

    Add users migration and update knexfile to work with pg

---
+++ api/data/migrations/20210326225120_create-users-table.js
@@ -0,0 +1,16 @@
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('users', (users) => {
+      users.increments('user_id')
+      users.string('username', 200).notNullable()
+      users.string('email', 200).notNullable()
+      users.string('password', 320).notNullable()
+      users.float('lat')
+      users.float('long')
+      users.timestamps(false, true)
+    })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('users')
+};
