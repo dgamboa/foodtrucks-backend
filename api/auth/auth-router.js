dada40aa1f2bd8e8dd62ae88a0773361b6f219c0
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-29 11:02:29 -0500

    Return user_id instead of id for user registration

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -24,7 +24,7 @@ router.post(
         .first();
       const token = buildToken(user);
       const registered = {
-        id: user.user_id,
+        user_id: user.user_id,
         username: user.username,
         email: user.email,
       };
