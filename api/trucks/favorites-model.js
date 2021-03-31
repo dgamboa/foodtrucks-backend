38d11b884459f60737bbc7c0d49a9c897673ff5c
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-31 16:41:45 -0500

    Implement DEL favorite truck model access methods

--- api/trucks/favorites-model.js
+++ api/trucks/favorites-model.js
@@ -8,4 +8,14 @@ function create(favorite) {
   ]);
 }
 
-module.exports = { create };
+async function remove(favorite_id) {
+  const favoriteToDelete = await getFavoriteById(favorite_id);
+  await db("favorites").where("favorite_id", favorite_id).del();
+  return favoriteToDelete;
+}
+
+function getFavoriteById(favorite_id) {
+  return db("favorites").where("favorite_id", favorite_id).first();
+}
+
+module.exports = { create, remove };
